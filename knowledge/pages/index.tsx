df2341bcc8013aeca791c6d5d3ff5777ae16ab2e
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-20 19:01:23 -0500

    new

--- knowledge/pages/index.tsx
+++ knowledge/pages/index.tsx
@@ -134,7 +134,7 @@ const HomePage: NextPage = () => {
   return (
     <PagesNavbar showSearch={!isIntersecting}>
       <HomeSearch sx={{ mt: "var(--navbar-height)" }} onSearch={handleSearch} ref={homeSearchRef} />
-      <Container sx={{ my: 8 }}>
+      <Container sx={{ my: 12 }}>
         <HomeFilter
           sx={{ mb: 8 }}
           onTagsChange={handleTagsChange}
